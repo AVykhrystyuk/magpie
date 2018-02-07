28793ea1c9c9dc1f3dde67757eac5f250f50418d
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2018-02-07 23:38:33 +0300

    updated html sanitizer and event interface

--- packages/timepad-magpie/src/impl/events-fetcher.js
+++ packages/timepad-magpie/src/impl/events-fetcher.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-irregular-whitespace,max-len */
 // @flow
 
 // lib
@@ -6,7 +7,7 @@ import {Injectable} from 'container-ioc';
 // app
 import TimePadEventsFetcher from '../events-fetcher';
 import ApiTimePadEventsFetcher from '../api/events-fetcher';
-import stripHtml from './utils/stripHtml';
+import sanitizeHtml, {stripHtml} from './utils/sanitize-html';
 import type {IApiTimePadEvent} from '../api/event.js.flow';
 import type {ITimePadEvent} from '../event.js.flow';
 
@@ -21,18 +22,30 @@ export default class TimePadEventsFetcherImpl extends TimePadEventsFetcher {
 
   async fetchEvents(): Promise<ITimePadEvent[]> {
     const apiEvents = await this._eventsFetcher.fetchEvents();
-    return apiEvents.map(TimePadEventsFetcherImpl._createEvent);
+    return apiEvents
+      .filter(TimePadEventsFetcherImpl._filterEvent)
+      .map(TimePadEventsFetcherImpl._createEvent);
+  }
+
+  static _filterEvent(apiEvent: IApiTimePadEvent): boolean {
+    const descriptionText = apiEvent.description_html
+      ? stripHtml(apiEvent.description_html)
+      : apiEvent.description_short;
+
+    return descriptionText != null;
   }
 
   static _createEvent(apiEvent: IApiTimePadEvent): ITimePadEvent {
-    const description = apiEvent.description_html
-      ? stripHtml(apiEvent.description_html)
-      : apiEvent.description_short;
+    const descriptionSanitizedHtml = apiEvent.description_html
+      ? sanitizeHtml(apiEvent.description_html)
+      : '';
 
     return {
       id: apiEvent.id,
       name: apiEvent.name,
-      description,
+      descriptionHtml: descriptionSanitizedHtml,
+      __description_html__: apiEvent.description_html,
+      __description_short__: apiEvent.description_short,
     };
   }
 }
---
+++ packages/timepad-magpie/src/impl/utils/sanitize-html.js
@@ -0,0 +1,32 @@
+// @flow
+
+// lib
+import sanitizeHtml from 'sanitize-html';
+
+// prettier-ignore
+const sanitizeOptions = {
+  allowedTags: [
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a',
+    'ul', 'ol', 'li',
+    'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br',
+    'div', // 'span'
+    'table', 'thead', 'tbody', 'tr', 'th', 'td',
+    'pre', 'caption'
+  ],
+  allowedAttributes: {
+    a: ['href']
+  },
+};
+
+const stripOptions = {
+  allowedTags: [],
+  allowedAttributes: [],
+};
+
+export function stripHtml(html: string) {
+  return sanitizeHtml(html, stripOptions);
+}
+
+export default function sanitize(html: string) {
+  return sanitizeHtml(html, sanitizeOptions);
+}
