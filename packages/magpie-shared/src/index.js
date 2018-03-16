5cd02f0e77b4908bb63dd17461c1ddeafe8a618c
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2018-03-16 13:58:30 +0500

    a try to use a "files" section in package.json instead of .npmignore file

--- packages/magpie-shared/src/index.js
+++ packages/magpie-shared/src/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 
+// prettier-ignore
 export {default as BlackListedWordsFinderFactory} from './black-listing/impl/black-listed-words-finder-factory';
 export {default as TagDetectorFactory} from './tag-detection/impl/tag-detector-factory';
