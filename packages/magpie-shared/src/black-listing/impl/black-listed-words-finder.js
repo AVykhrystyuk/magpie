e0fa335f1c99bc3d33cdfa492eeeadd77269a96b
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2018-05-29 01:05:24 +0300

    got rid of an assignment in while loop

--- packages/magpie-shared/src/black-listing/impl/black-listed-words-finder.js
+++ packages/magpie-shared/src/black-listing/impl/black-listed-words-finder.js
@@ -12,19 +12,33 @@ export default class BlackListedWordsFinderImpl extends BlackListedWordsFinder {
     /предприниматель[а-яё]*/gi, // предприниматель(ство)?
     /трудоустро[а-яё]+/gi, // трудоустро(йство|ить)?
     /ваканси(и|я)/gi, // вакансии, вакансия
-    /семинары?/gi
+    /семинары?/gi,
   ];
 
   find(text: string): string[] {
     const foundWords = [];
 
     for (const regExp of this._regExps) {
-      let match = null;
-      while ((match = regExp.exec(text)) !== null) {
-        foundWords.push(match[0]);
-      }
+      const matches = this._findAllMatches(text, regExp);
+      foundWords.push(...matches);
     }
 
     return foundWords;
+  }
+
+  _findAllMatches(text: string, regExp: RegExp): string[] {
+    const matches = [];
+
+    let loop = true;
+    do {
+      const match = regExp.exec(text);
+      if (match != null) {
+        matches.push(match[0]);
+      } else {
+        loop = false;
+      }
+    } while (loop);
+
+    return matches;
   }
 }
