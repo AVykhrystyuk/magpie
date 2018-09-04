31e12b9149b17e4899738ef821484735a7a91afd
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2018-09-05 00:05:58 +0300

    Add iosTag to tagDetector

--- packages/magpie-shared/src/tag-analysis/tag-detection/create-tag-detector.js
+++ packages/magpie-shared/src/tag-analysis/tag-detection/create-tag-detector.js
@@ -2,16 +2,17 @@
 // @flow
 
 // app
-import TagDetector from '../tag-detector';
-import TagDetectorImpl from './tag-detector';
-import { JsTag, DotNetTag, FrontEndTag } from './tags/index';
+import TagDetector from './tag-detector';
+import TagDetectorImpl from './impl/tag-detector';
+import { DotNetTag, FrontEndTag, IosTag, JsTag } from './impl/tags';
 
 export default function createTagDetector(): TagDetector {
   // prettier-ignore
   const tags = [
-    new JsTag(),
     new DotNetTag(),
-    new FrontEndTag()
+    new FrontEndTag(),
+    new IosTag(),
+    new JsTag(),
   ];
   return new TagDetectorImpl(tags);
 }
