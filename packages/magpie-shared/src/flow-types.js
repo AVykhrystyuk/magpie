7c01510c31c7be0eb397893006c3d82220c0fbca
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2018-09-06 22:49:54 +0300

    Add flow-types typings for library users

---
+++ packages/magpie-shared/src/flow-types.js
@@ -0,0 +1,46 @@
+/* eslint-disable flowtype/no-types-missing-file-annotation */
+
+declare module 'magpie-shared' {
+
+  declare export type TagAnalysisResult = {|
+    valid: boolean,
+    whiteWords: string[],
+    blackWords: string[],
+    tagIds: string[],
+  |};
+
+  declare export class TagAnalyzer {
+    analize(text: string): TagAnalysisResult;
+  }
+
+  declare export function createTagAnalyzer(): TagAnalyzer;
+
+
+
+  declare type TagId = string;
+
+  declare export class TagDetector {
+    detectAll(text: string): TagId[];
+  }
+
+  declare export function createTagDetector(): TagDetector;
+
+
+
+  declare export class BlackListedWordsFinder {
+    findAll(text: string): string[];
+    findOne(text: string): ?string;
+  }
+
+  declare export function createBlackListedWordsFinder(): BlackListedWordsFinder;
+
+
+
+  declare export class WhiteListedWordsFinder {
+    findAll(text: string): string[];
+    findOne(text: string): ?string;
+  }
+
+  declare export function createWhiteListedWordsFinder(): WhiteListedWordsFinder;
+
+}
