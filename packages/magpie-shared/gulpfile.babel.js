3f6d9a58473175faf8b5bef8abf0c884850080f5
Alexey Vykhrystyuk <AVykhrystyuk@users.noreply.github.com>
2019-02-27 00:56:36 +0300

    Merge pull request #19 from AVykhrystyuk/feature/use-gulp
    Start using gulp for build tasks

---
+++ packages/magpie-shared/gulpfile.babel.js
@@ -0,0 +1,98 @@
+/* eslint-disable no-console */
+import gulp from 'gulp';
+import sourcemaps from 'gulp-sourcemaps';
+import babel from 'gulp-babel';
+import eslint from 'gulp-eslint';
+import _if from 'gulp-if';
+import rimraf from 'rimraf';
+import { promisify } from 'util';
+
+const rimrafPromised = promisify(rimraf);
+
+function getEnvOptions() {
+  const argv = process.argv.slice(2);
+  const isProduction = argv.includes('--production');
+  const eslintShouldFix = argv.includes('--fix');
+  return {
+    isProduction,
+    eslintShouldFix,
+    sourcemaps: !isProduction,
+  };
+}
+
+function getPaths() {
+  const commonDest = 'dist/';
+  const preparedFlowFiles = 'src/prepared-flow-files/*.{js,js.flow}';
+
+  return {
+    dest: commonDest,
+    javascript: {
+      src: [
+        'src/**/*.{js,js.flow}',
+        '!src/**/*.spec.js',
+        `!${preparedFlowFiles}`
+      ],
+      dest: commonDest,
+      fixDest: 'src',
+    },
+    preparedFlowFiles: {
+      src: preparedFlowFiles,
+      dest: commonDest,
+    },
+  };
+}
+
+const envOptions = getEnvOptions();
+const paths = getPaths();
+
+console.log('envOptions:', envOptions);
+
+export function lint() {
+  const isFileFixed = file => file.eslint != null && file.eslint.fixed;
+  const isFileNotFixed = file => !isFileFixed(file);
+
+  return gulp
+    .src(paths.javascript.src)
+    .pipe(
+      eslint({
+        fix: envOptions.eslintShouldFix,
+      })
+    )
+    .pipe(eslint.format())
+    .pipe(_if(isFileNotFixed, eslint.failAfterError()))
+    .pipe(_if(isFileFixed, gulp.dest(paths.javascript.fixDest)));
+}
+
+export function clean() {
+  return rimrafPromised(paths.dest);
+}
+
+export function buildJavaScript() {
+  return gulp
+    .src(paths.javascript.src)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(_if(envOptions.isProduction, eslint.failAfterError()))
+    .pipe(_if(envOptions.sourcemaps, sourcemaps.init()))
+    .pipe(babel())
+    .pipe(_if(envOptions.sourcemaps, sourcemaps.write('./.maps')))
+    .pipe(gulp.dest(paths.javascript.dest));
+}
+
+export function copyPreparedFlowFiles() {
+  return gulp
+    .src(paths.preparedFlowFiles.src)
+    .pipe(gulp.dest(paths.preparedFlowFiles.dest));
+}
+
+export function watch() {
+  const onFilesChange = path => {
+    console.log(`File '${path}' was changed, running tasks...`);
+  };
+
+  gulp.watch(paths.javascript.src, buildJavaScript).on('change', onFilesChange);
+}
+
+export const build = gulp.series(clean, gulp.parallel([buildJavaScript, copyPreparedFlowFiles]));
+
+export default build;
--- packages/magpie-shared/src/utils/is-whitespace-or-empty.js
+++ packages/magpie-shared/src/utils/is-whitespace-or-empty.js
@@ -1,11 +1,7 @@
 // @flow
 
-const blankStringRegexp = /^\s*$/;
+const blankTextRegexp = /^\s*$/;
 
 export default function isWhitespaceOrEmpty(value: string): boolean {
-  return !value || blankStringRegexp.test(value);
+  return !value || blankTextRegexp.test(value);
 }
-
-// export default function isWhitespaceOrEmpty(value: string): boolean {
-//   return !value || value.trim().length === 0;
-// }
