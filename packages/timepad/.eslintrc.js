889be20c73fd31574b4ccfc8df9d9ddcad9f48f4
Alexey Vykhrystyuk <AVykhrystyuk@gmail.com>
2019-05-17 12:40:16 +0300

    chore: add package with common eslint rules

--- packages/timepad/.eslintrc.js
+++ packages/timepad/.eslintrc.js
@@ -4,67 +4,6 @@ module.exports = {
   },
 
   extends: [
-    'airbnb-base',
-    'plugin:flowtype/recommended'
+    require.resolve('@magpie/code-style/.flowtype.eslintrc.js')
   ],
-
-  plugins: [
-    'flowtype'
-  ],
-
-  rules: {
-    /* es6 related */
-    // https://eslint.org/docs/rules/arrow-parens
-    'arrow-parens': ['error', 'as-needed', {
-      requireForBlockBody: false,
-    }],
-
-    /* styles related */
-    // prettier takes care of it
-    // https://eslint.org/docs/rules/object-curly-newline
-    'object-curly-newline': ['off', {
-      ObjectExpression: { minProperties: 4, multiline: true, consistent: true },
-      ObjectPattern: { minProperties: 4, multiline: true, consistent: true },
-      ImportDeclaration: { minProperties: 4, multiline: true, consistent: true },
-      ExportDeclaration: { minProperties: 4, multiline: true, consistent: true },
-    }],
-
-    // https://eslint.org/docs/rules/no-underscore-dangle
-    'no-underscore-dangle': ['error', {
-      allow: [],
-      allowAfterThis: true,
-      allowAfterSuper: false,
-      enforceInMethodNames: false,
-    }],
-
-    // https://eslint.org/docs/rules/max-len
-    'max-len': ['error', {
-      code: 120,
-      tabWidth: 4,
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-    }],
-
-    // https://eslint.org/docs/rules/comma-dangle
-    'comma-dangle': ['error', {
-      arrays: 'only-multiline',
-      objects: 'only-multiline',
-      imports: 'only-multiline',
-      exports: 'never',
-      functions: 'never',
-    }],
-
-    // https://eslint.org/docs/rules/newline-per-chained-call
-    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
-
-    // https://eslint.org/docs/rules/lines-between-class-members
-    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
-
-    // disallow use of unary operators, ++ and --
-    // https://eslint.org/docs/rules/no-plusplus
-    'no-plusplus': ["error", { "allowForLoopAfterthoughts": true }],
-  }
 };
